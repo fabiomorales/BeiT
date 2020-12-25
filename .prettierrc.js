1774d60432ae162941b29e5eef516216cbb82746
Fabio Luiz Morales <fabio.luiz.morales@avanade.com>
2020-12-25 14:55:36 -0300

    feat(initial commit): create app

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  tabWidth: 2,
+  semi: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Wold</Text>
+    </View>
+  );
+};
+
+export default App;
